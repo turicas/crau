3298da2edba31a29cc352085fbe0aced27bfabb5
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2022-01-27 10:59:46 -0300

    Bump to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="crau",
     description="Easy-to-use Web archiver",
-    version="0.1.0",
+    version="0.1.1",
     author="Álvaro Justen",
     author_email="alvarojusten@gmail.com",
     url="https://github.com/turicas/crau/",
