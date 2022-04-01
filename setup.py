6e1b924d1393c95ae3d2cff3cc7ee7512430fa38
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2022-04-01 01:03:35 -0300

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="crau",
     description="Easy-to-use Web archiver",
-    version="0.1.1dev0",
+    version="0.2.0",
     author="Álvaro Justen",
     author_email="alvarojusten@gmail.com",
     url="https://github.com/turicas/crau/",
